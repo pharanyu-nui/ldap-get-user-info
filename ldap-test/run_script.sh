a4b13e1cfe3dfdb62aa6c36a33e6bc4dd018fba8
pharanyu <pharanyu.c@codium.co>
2021-10-14 12:09:09 +0700

    add new ldap server and fixed code

--- ldap-test/run_script.sh
+++ ldap-test/run_script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-python3 ldap_get_info.py --host="openldap" \
-  --port="389" \
-  --user="admin" \
-  --pass="admin_pass" \
-  --base="dc=ramhlocal,dc=com"
+python3 ldap_get_info.py --host="ldapad" \
+  --port="10389" \
+  --user="uid=test,ou=users,dc=wimpi,dc=net" \
+  --pass="secret" \
+  --base="dc=wimpi,dc=net"
+
+# python3 ldap_get_info.py --host="openldap" \
+#   --port="389" \
+#   --user="cn=admin,dc=ramhlocal,dc=com" \
+#   --pass="admin_pass" \
+#   --base="dc=ramhlocal,dc=com"
